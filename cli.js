0652dc7929c1ca074b21826e7ce86ae57ba8f23f
Rob Wettach <robwettach@gmail.com>
2018-12-01 18:30:29 -0700

    Consolidate edges by default, add 'expand' flag
    Resolves #3

--- cli.js
+++ cli.js
@@ -5,7 +5,8 @@ const commandLineArgs = require('command-line-args');
 const optionDefinitions = [
   { name: 'verbose', alias: 'v', type: Boolean },
   { name: 'output', alias: 'o', type: String, defaultOption: true },
-  { name: 'label', alias: 'l', type: Boolean }
+  { name: 'label', alias: 'l', type: Boolean },
+  { name: 'expand', alias: 'e', type: Boolean }
 ]
 
 let options = commandLineArgs(optionDefinitions);
--- index.js
+++ index.js
@@ -4,23 +4,19 @@ const GraphViz = require('graphviz');
 // The AWS SDK reads the default credentials file, but not the config file unless you tell it to
 process.env.AWS_SDK_LOAD_CONFIG = true;
 var cfn = new AWS.CloudFormation();
-var g = GraphViz.digraph("G");
-
-async function asyncForEach(array, callback) {
-  for (let index = 0; index < array.length; index++) {
-    await callback(array[index], index, array);
-  }
-}
+const g = GraphViz.digraph("G");
 
 const DEFAULT_OPTIONS = {
-  label: false
+  label: false,
+  expand: false
 };
 
-module.exports = async function renderDependencies(fileNameOrOptions) {
+function buildOptions(fileNameOrOptions) {
   let options;
   if (typeof fileNameOrOptions === 'string') {
-    options = Object.assign({}, {...DEFAULT_OPTIONS, output: fileNameOrOptions});
-  } else {
+    options = Object.assign({}, { ...DEFAULT_OPTIONS, output: fileNameOrOptions });
+  }
+  else {
     options = Object.assign({}, DEFAULT_OPTIONS, fileNameOrOptions);
   }
   if (!options.output) {
@@ -32,6 +28,12 @@ module.exports = async function renderDependencies(fileNameOrOptions) {
       console.log(str);
     }
   }
+
+  return [options, log];
+}
+
+module.exports = async function renderDependencies(fileNameOrOptions) {
+  let [options, log] = buildOptions(fileNameOrOptions);
 
   var stackNames = (await cfn.listStacks().promise()).StackSummaries.map(s => s.StackName);
   log(`Found ${stackNames.length} Stacks`);
@@ -45,27 +47,59 @@ module.exports = async function renderDependencies(fileNameOrOptions) {
   });
   log(`Found ${exports.length} Exports`);
 
-  await asyncForEach(exports, async e => {
+  await Promise.all(exports.map(async e => {
     try {
       var imports = (await cfn.listImports({ExportName: e.name}).promise()).Imports;
       log(`Found ${imports.length} Imports for ${e.name}`);
       imports.forEach(i => {
-        const edgeOptions = {};
-        if (options.label) {
-          edgeOptions.label = e.name;
-        }
-        g.addEdge(e.stackName, i, edgeOptions);
+        addEdge(options, e, i);
       })
     } catch (ex) {
       // Ignore exceptions that an export isn't imported anywhere
       log(`No Imports for ${e.name}`);
     }
-  });
+  }));
 
   if (g.edgeCount() > 0) {
     g.render("png", options.output);
     console.log(`Rendered stack dependencies to ${options.output}`);
   } else {
     console.log("No cross-stack dependencies found!");
+  }
+}
+
+const KNOWN_EDGES = {}
+function getEdge(g, nodeOne, nodeTwo) {
+  const edgeKey = `${nodeOne}->${nodeTwo}`;
+  const cachedEdge = KNOWN_EDGES[edgeKey];
+  if (cachedEdge) {
+    return cachedEdge;
+  } else {
+    const edge = g.edges.find(edge => edge.nodeOne.id === nodeOne && edge.nodeTwo.id === nodeTwo);
+    if (edge) {
+      KNOWN_EDGES[edgeKey] = edge;
+    }
+    return edge;
+  }
+}
+
+function addEdge(options, exprt, imprt) {
+  const edgeOptions = {};
+  if (options.label) {
+    edgeOptions.label = exprt.name;
+  }
+  let shouldAdd = true;
+  if (!options.expand) {
+    const existingEdge = getEdge(g, exprt.stackName, imprt);
+    if (existingEdge) {
+      const existingLabel = existingEdge.get("label");
+      if (existingLabel && edgeOptions.label) {
+        existingEdge.set("label", `${existingLabel}\n${edgeOptions.label}`);
+      }
+    }
+    shouldAdd = !existingEdge;
+  }
+  if (shouldAdd) {
+    g.addEdge(exprt.stackName, imprt, edgeOptions);
   }
 }
